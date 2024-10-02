e7d89b4bb11fea3d6233dd5e383e217941a37348
devmikas <devlucasmikas01@gmail.com>
2024-10-02 12:33:34 -0300

    exerde javascript
    cursu  em video e trybe

---
+++ ex01.js
@@ -0,0 +1,10 @@
+//var vel = 60.1;
+
+var pais = 'EUA';
+console.log(`Vivendo em ${pais}`);
+if (pais=='Brasil'){
+    console.log('voece e Brasil !');
+}
+    else{
+    console.log('voce e estrangeiro!');
+}
